d21e6f6feee513f6d716a3df0c2f00601d2b2bd2
jardellx <>
2026-04-24 13:18:07 -0300

    fix: pixel manual trigger

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,24 +15,25 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "MultiTech Magalhães | Inovação ao seu alcance",
-  description: "Os melhores gadgets e smartwatches do mercado.",
+  title: "MultiTech Magalhães",
+  description: "Inovação ao seu alcance.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="pt-BR">
       <head>
+        {/* PIXEL DO TIKTOK - MODO DE SEGURANÇA */}
         <Script id="tiktok-pixel" strategy="afterInteractive">
           {`
             !function (w, d, t) {
               w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","trackWithQuery","click","updateCookie"],ttq.setAndGetCookie=function(t,e){var n=ttq.instances[t];n&&n.setAndGetCookie(e)},ttq.instance=function(t){for(var e=ttq.instances[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndGetCookie(e,ttq.methods[n]);return e},ttq.load=function(e,n){var t="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=t,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n;var i=d.createElement("script");i.type="text/javascript",i.async=!0,i.src=t+"?sdkid="+e+"&lib="+t;var a=d.getElementsByTagName("script")[0];a.parentNode.insertBefore(i,a)};
             
-              ttq.load('D7LNUKRC77U471PGTR70'); 
+              ttq.load('COLE_SEU_ID_AQUI'); // <--- VERIFIQUE SE O ID ESTÁ CERTO AQUI
               ttq.page();
             }(window, document, 'ttq');
           `}
